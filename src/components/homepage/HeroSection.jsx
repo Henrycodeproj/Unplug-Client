cf1a15649a78b0ba3c5c910faa06c775291c8c40
Henry <lihenryhl@yahoo.com>
2022-12-14 13:27:02 -0800

    fixing tooltip fonts

--- src/components/homepage/HeroSection.jsx
+++ src/components/homepage/HeroSection.jsx
@@ -33,28 +33,48 @@ export const HeroSection = ({setOption, setClicked}) => {
               <motion.div
               whileHover={{scale:1.3, y:-10}}
               >
-                <Tooltip title = "Boardgame night in my dorm tonight!" TransitionComponent={Zoom} placement="top" arrow>
+                <Tooltip 
+                title = {<h6 style={{ fontSize:"1rem" }}>Boardgame night in my dorm tonight!</h6>}
+                TransitionComponent={Zoom} 
+                placement="top" 
+                arrow
+                >
                 <Avatar src = "https://images.pexels.com/photos/3781529/pexels-photo-3781529.jpeg?auto=compress&cs=tinysrgb&w=1600" sx = {avatarStyling}/>
                 </Tooltip>
               </motion.div>
               <motion.div
               whileHover={{scale:1.3, y:-10}}
               >
-                <Tooltip title="Looking for a buddy to study Physics 1A with!" TransitionComponent={Zoom} placement="top" arrow>
+                <Tooltip 
+                title = {<h6 style={{ fontSize:"1rem" }}>Looking for a buddy to study Physics 1A with!</h6>} 
+                TransitionComponent={Zoom} 
+                placement="top" 
+                arrow
+                >
                   <Avatar src = "https://images.pexels.com/photos/3762800/pexels-photo-3762800.jpeg?auto=compress&cs=tinysrgb&w=1600" sx = {avatarStyling}/>
                 </Tooltip>
               </motion.div>
               <motion.div
               whileHover={{scale:1.3, y:-10}}
               >
-                <Tooltip title = "Grabbing lunch tomorrow if anyone wants join me!" TransitionComponent={Zoom} placement="top" arrow>
+                <Tooltip 
+                title = {<h6 style={{ fontSize:"1rem" }}>Grabbing lunch tomorrow if anyone wants join me!</h6>}
+                TransitionComponent={Zoom} 
+                placement="top" 
+                arrow
+                >
                   <Avatar src = "https://images.pexels.com/photos/5537546/pexels-photo-5537546.jpeg?auto=compress&cs=tinysrgb&w=1600" sx = {avatarStyling}/>
                 </Tooltip>
               </motion.div>
               <motion.div
               whileHover={{scale:1.3, y:-10}}
               >
-                <Tooltip title = "Playing Pool at 5:30 on the east of campus today!" TransitionComponent={Zoom} placement="top" arrow>
+                <Tooltip 
+                title = {<h6 style={{ fontSize:"1rem" }}>Playing Pool at 5:30 on the east of campus today!</h6>}
+                TransitionComponent={Zoom} 
+                placement="top" 
+                arrow
+                >
                   <Avatar src = "https://images.pexels.com/photos/13798632/pexels-photo-13798632.jpeg?auto=compress&cs=tinysrgb&w=1600" sx = {avatarStyling}/>
                 </Tooltip>
               </motion.div>
